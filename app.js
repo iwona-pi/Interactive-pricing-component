be6d03a6d33f1bd65f1e4ca5bdec578938210f36
Iwona <iwoda3@gmail.com>
2021-07-06 15:01:13 +0200

    Created var model in app.js file

---
+++ app.js
@@ -0,0 +1,35 @@
+var model = {
+	data:[
+	{
+		views: 10000,
+		show: '10k',
+		price: '$8'
+
+		},
+	
+	{	
+		views: 50000,
+		show: '50k',
+		price: '$12'
+		
+		},
+{	
+		views: 100000,
+		show: '100k',
+		price: '$16'
+		
+		},
+{	
+		views: 500000,
+		show: '500k',
+		price: '$24'
+		
+		},		
+{	
+		views: 1000000,
+		show: '1M',
+		price: '$36'
+		
+		},		
+		]				
+};

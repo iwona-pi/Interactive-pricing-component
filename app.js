e70d93c99a3cedd9a8598134101ab213683eb794
Iwona <iwoda3@gmail.com>
2021-10-22 00:23:20 +0200

    Successfully cretaed function changing number of views

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var model = {
+	currentView: null,
 	data:[
 	{
 		views: 10000,
@@ -33,3 +34,61 @@ var model = {
 		},		
 		]				
 };
+
+/*var octopus = {
+	//let val = document.getElementById("cowbell").value;
+	getprice: function() {
+		this.val = document.getElementById("cowbell").value;
+		for (i=0; i< model.data.length; i++) {
+			if model.data[i].views =this.val;
+		} document.getElementsByClassName("number_views").innerText = val
+	}
+};*/
+var octopus = {
+	getData: function() {
+		return model.data
+	},
+	init: function() {
+		model.currentView = model.data[0];
+		
+		
+		PagePriceView.init();
+},
+	setcurrentView: function(data) {
+		model.currentView = data;
+	},
+	getcurrentView: function() {
+		return model.currentView;
+	}
+}
+
+var PagePriceView = {
+	init: function() {
+		var currentView = octopus.getcurrentView();
+		
+		this.ViewsElem = document.getElementById("number_views");
+		this.ViewsElem.innerText = currentView.show;
+		//this.val = document.getElementById("cowbell").value;
+		this.input = document.getElementById("cowbell");
+
+		this.render();
+	},
+
+	render: function() {
+		
+		var data = octopus.getData();
+		 
+		this.input.onchange= function() {
+			for (i=0; i<data.length; i++)
+			{ if (data[i].views <= this.value )
+				//|| data[i+1] >= this.value)
+				{octopus.setcurrentView(data[i]);
+					PagePriceView.init();
+				};
+			}
+		}
+	}
+
+};
+
+octopus.init();
